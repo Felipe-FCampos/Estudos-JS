1c9b5ff1549f1c849aa6044afa24672e25195abb
Felipe-FCampos <felipe.fcampos7@gmail.com>
2023-02-13 20:56:01 -0300

    a

---
+++ script.js
@@ -0,0 +1,12 @@
+const numero = Number(prompt("num 1: "))
+const numero2 = Number(prompt("num 2: "))
+
+const soma = numero + numero2
+
+if(soma > 10 ){
+    alert('Este número é maior que 10.' + ' E o resultado é: ' + soma)
+} if(soma == 10){
+    alert('Este número é igual a 10.' + ' E o resultado é: ' + soma)
+} else {
+    alert('Este número é menor que 10.' + ' E o resultado é: ' + soma)
+}
